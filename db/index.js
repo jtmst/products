b38a5f5862166498a45193d9d4866b163410fa86
jtmst <josh.troy.mills@gmail.com>
2020-11-30 20:14:30 -0500

    db init

---
+++ db/index.js
@@ -0,0 +1,18 @@
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/products');
+
+// let productsSchema = mongoose.Schema({
+//     name: String,
+//     slogan: String,
+//     description: String,
+//     category: String,
+//     default_price: String,
+//     sale_price: String,
+//     related_products: [Number],
+//     features: [{
+//         feature: String,
+//         value: String
+//     }],
+//     styles: 
+
+// })
