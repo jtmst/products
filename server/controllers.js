24f04ee3964fb8faa60d6b312edf8e6904d16a87
jtmst <josh.troy.mills@gmail.com>
2020-12-02 19:30:33 -0500

    database setup with router/controller

--- server/controllers.js
+++ server/controllers.js
@@ -1,10 +1,26 @@
-const models = require("./models")
+const db = require("../db/index")
 
 module.exports = {
     getList: function (req, res) {
-        console.log('getList')
+        let { count, page } = req.query
+        db.productListFind(count, page)
+            .then(list => {
+                res.status(200).send(list)
+            })
     },
-    getProduct: function (req, res) { },
-    getStyles: function (req, res) { },
+    getProduct: function (req, res) {
+        let { product_id } = req.params
+        db.productIdSearch(product_id)
+            .then(product => {
+                res.status(200).send(product)
+            })
+    },
+    getStyles: function (req, res) {
+        let { product_id } = req.params
+        db.stylesByProdId(product_id)
+            .then(styles => {
+                res.status(200).send(styles)
+            })
+    },
     getRelated: function (req, res) { }
 }
